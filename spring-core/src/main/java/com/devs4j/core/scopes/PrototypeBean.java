f2482b992c36dd76916c3c27890ce4b83c4ef100
Alejandro Agapito Bautista <alejandro.agapito@walmart.com>
2019-05-16 15:36:52 -0500

    scopes

---
+++ spring-core/src/main/java/com/devs4j/core/scopes/PrototypeBean.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.devs4j.core.scopes;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author maagapi
+ *
+ */
+@Component
+@Scope("prototype")
+public class PrototypeBean {
+
+}
